25fb71b45263613146795c73b3cdb5d76e8ccc45
Edip SACİ <saciedip@gmail.com>
2022-12-14 12:54:40 +0300

    day38 HAS a Relation

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,14 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota= new Toyota("Camry","blue",2020,12358,20000);
+
+        Tesla tesla= new Tesla("Model S","black",2022,0,94990);
+
+        BMW bmw =  new BMW("X5","white",2019,1244,19000);
+
+    }
+}
