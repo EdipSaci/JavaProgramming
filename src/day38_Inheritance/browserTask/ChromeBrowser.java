3ecab458ce84042e1f2e48dbb67544799f6cf06f
Edip SACİ <saciedip@gmail.com>
2022-12-15 12:58:21 +0300

    day38 Browser task is done

---
+++ src/day38_Inheritance/browserTask/ChromeBrowser.java
@@ -0,0 +1,13 @@
+package day38_Inheritance.browserTask;
+
+public class ChromeBrowser extends Browser{
+    @Override
+    public void openBrowser() {
+        super.openBrowser();
+    }
+
+    @Override
+    public void closeBrowser() {
+        super.closeBrowser();
+    }
+}
