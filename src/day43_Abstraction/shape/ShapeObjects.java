ce3f440375d8e46188bc170b623f031015ca6a35
Edip SACİ <saciedip@gmail.com>
2022-12-30 19:10:47 +0300

    day43 Shape homework task done

---
+++ src/day43_Abstraction/shape/ShapeObjects.java
@@ -0,0 +1,17 @@
+package day43_Abstraction.shape;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle= new Rectangle(5,7.2);
+        System.out.println(rectangle);
+
+        Square square= new Square(9.4);
+        System.out.println(square);
+
+        Circle circle= new Circle(2.1);
+        System.out.println(circle);
+
+    }
+}
