7e9dacf28c5ed176fb8660c85e4310f1dd31260a
Edip SACİ <saciedip@gmail.com>
2023-01-02 12:29:38 +0300

    day44 Animal task

---
+++ src/day44_Abstraction/animalTask/Dog.java
@@ -0,0 +1,20 @@
+package day44_Abstraction.animalTask;
+
+public final class Dog  extends Animal{
+
+    public Dog(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName()+ " is eats pizza ");
+    }
+
+    public void bark(){
+        System.out.println(getName()+ " is barking");
+    }
+
+
+
+}
